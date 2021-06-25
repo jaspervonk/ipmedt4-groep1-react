0c0284a80cc79b610ab3e4fc4e3bb64ee0cec49d
Max <s1121610@student.hsleiden.nl>
2021-06-25 13:55:00 +0200

    Errorfix boekdetails met timeout

--- src/boekenlijst/Components/DetailHoofdstuk.js
+++ src/boekenlijst/Components/DetailHoofdstuk.js
@@ -7,6 +7,20 @@ class DetailHoofdstuk extends React.Component {
         this.props.cardClicked(this.props.id);
     }
 
+    componentDidUpdate = () => {
+        // krijg gelezenHoofdstukken binnen via props (altijd)
+
+
+        // check of deze niet leeg is, anders skip .checked=true
+        // if this.props.gelezenHoofdstukken yadayada
+        // for(var i = 0; i < gelezenHoofdstukken.length; i++){
+        //     document.getElementById(gelezenHoofdstukken[i].hoofdstuk_id).checked=true;
+        //     if(document.getElementById(i + "K")){
+        //         document.getElementById(i + "K").innerHTML="uitdaging";
+        //     }
+        // }
+    }
+
     //Elk hoofdstuk als component, checkbox met id van hoofdstuk_id
     render(){
         return(
